9c781ac4f5d96b1a83a7a16116e0d33ef39e30ac
nojebar <nicolas.ojeda.bar@lexifi.com>
2023-05-15 09:48:37 +0200

    Begin

---
+++ examples/7guis/1-counter/counter.ml
@@ -0,0 +1,24 @@
+type model =
+  int
+
+type msg =
+  | Clicked
+
+let init =
+  0
+
+let view n =
+  Vdom.div [
+    Vdom.input ~a:[Vdom.value (string_of_int n); Vdom.bool_prop "readOnly" true] [];
+    Vdom.elt "button" ~a:[Vdom.type_button; Vdom.onclick (fun _ -> Clicked)] []
+  ]
+
+let update (model : model) Clicked =
+  model + 1
+
+let _ =
+  let app = Vdom.simple_app ~init ~update ~view () in
+  let container =
+    Option.get (Js_browser.Document.get_element_by_id Js_browser.document "container")
+  in
+  Vdom_blit.dom (Vdom_blit.run ~container app)
